1f3f5f21169da73f2f540beb36dc164025dd79e8
Giulia Barbizan <giuliabarbizan@gmail.com>
2025-09-19 12:14:33 -0300

    Referenciando os posts do usuario

--- src/main/java/com/exemplo/mongodbproject/domain/User.java
+++ src/main/java/com/exemplo/mongodbproject/domain/User.java
@@ -2,20 +2,32 @@ package com.exemplo.mongodbproject.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Document(collection = "user")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User implements Serializable {
     @Id
     private String id;
     private String name;
     private String email;
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 
 }
