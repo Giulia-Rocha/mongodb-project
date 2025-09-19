132c2d752f159c0d986d1f2d0e5f27fa3dd2d479
Giulia Barbizan <giuliabarbizan@gmail.com>
2025-09-19 12:30:27 -0300

    Obtendo post por id

--- src/main/java/com/exemplo/mongodbproject/repository/PostRepository.java
+++ src/main/java/com/exemplo/mongodbproject/repository/PostRepository.java
@@ -1,7 +1,6 @@
 package com.exemplo.mongodbproject.repository;
 
 import com.exemplo.mongodbproject.domain.Post;
-import com.exemplo.mongodbproject.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
---
+++ src/main/java/com/exemplo/mongodbproject/services/PostService.java
@@ -0,0 +1,21 @@
+package com.exemplo.mongodbproject.services;
+
+import com.exemplo.mongodbproject.domain.Post;
+import com.exemplo.mongodbproject.domain.User;
+import com.exemplo.mongodbproject.repository.PostRepository;
+import com.exemplo.mongodbproject.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id){
+        Optional<Post> obj = postRepository.findById(id);
+        return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto Não Encontrado"));
+    }
+
+}
